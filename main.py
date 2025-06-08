b8c02dbe17efed4ea5850a8d06b44eca36209b78
Protonanamous <garrick.a.glover@gmail.com>
2025-06-08 11:27:31 -0500

    added sys args to the main program and added two books

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-
-from stats import get_num_words, get_book_text
+import sys
+from stats import get_num_words, char_freq
 
 def main():
-    get_num_words()
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
 
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {sys.argv[1]}")
+    print("-----------Word Count -----------")
+    
+    get_num_words(sys.argv[1])
+    
+    print("---------Character Count --------")
+    char_freq(sys.argv[1])
+
+    
 main()
--- stats.py
+++ stats.py
@@ -3,10 +3,38 @@ def get_book_text(filepath):
         file_contents = f.read()
     return file_contents
 
-def get_num_words():
+def get_num_words(filepath):
 
     num = 0
-    text = get_book_text("/home/gg/bootdev/bookbot/books/frankenstein.txt")
+    text = get_book_text(filepath)
     for i in text.split():
         num += 1
-    print(f"{num} words found in the document")
+    print(f"Found {num} total words")
+
+def char_freq(filepath):
+    count = 0
+    char_dict = {}
+    list_dict = []
+    text = get_book_text(filepath).lower()
+    for i in text:
+        if i.isalpha():
+            if i in char_dict:
+                char_dict[i] = char_dict[i] + 1
+            else:
+                char_dict[i] = 1
+    for i in char_dict:
+        list_dict.append({"name": i, "num": char_dict[i]})
+    list_dict.sort(reverse=True, key=sort_on)
+    
+    for i in list_dict:
+        
+        print(f"{list_dict[count]['name']}: {list_dict[count]['num']}")
+        count += 1
+
+
+# A function that takes a dictionary and returns the value of the "num" key
+# This is how the `.sort()` method knows how to sort the list of dictionaries
+def sort_on(dict):
+    return dict["num"]
+
+
